7d9e0d4e36e6bfd8baff284cfab3b0f49a615909
Olgierd Palasz <olgierd.palasz@gmail.com>
2024-12-01 16:06:21 +0100

    feat: Template for justfile

---
+++ template/main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Println("Advent of Code 2024!")
+	Part1()
+	// Part2()
+}
+
+func Part1() {
+	fmt.Println("Part 1")
+}
+
+func Part2() {
+	fmt.Println("Part 2")
+}
