735fb5dd78b647931eda8fe8711cf0c2486c634b
Olgierd Palasz <olgierd.palasz@gmail.com>
2024-12-01 16:05:34 +0100

    feat: Atoi error handler function and function to read file as one string

---
+++ cmd/utils/utils.go
@@ -0,0 +1,25 @@
+package utils
+
+import (
+	"fmt"
+	"os"
+	"strconv"
+)
+
+func MustAtoi(s string) int {
+	i, err := strconv.Atoi(s)
+	if err != nil {
+		panic(fmt.Sprintf("failed to convert %q to int: %v", s, err))
+	}
+	return i
+}
+
+func ReadFile(path string) string {
+	lines, err := os.ReadFile(path)
+
+	if err != nil {
+		panic(fmt.Sprintf("failed to read file %q: %v", path, err))
+	}
+
+	return string(lines)
+}
