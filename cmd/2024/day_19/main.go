5ec875c1276207b46f6f410ad086b00a3e46a184
Olgierd Palasz <olgierd.palasz@gmail.com>
2024-12-20 23:55:00 +0100

    feat: day 18 - parser

---
+++ cmd/2024/day_19/main.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+)
+
+func main() {
+	fmt.Println("Advent of Code 2024!")
+	Part1()
+	// Part2()
+}
+
+func Part1() {
+	fmt.Println("Part 1")
+
+	input := `r, wr, b, g, bwu, rb, gb, br
+
+brwrr
+bggr
+gbbr
+rrbgbr
+ubwu
+bwurrg
+brgr
+bbrgwb`
+
+	patterns := strings.Split(strings.Split(input, "\n")[0], ", ")
+
+	designs := []string{}
+
+	for _, row := range strings.Split(input, "\n")[1:] {
+		if row != "" {
+			designs = append(designs, row)
+		}
+	}
+
+	fmt.Println(patterns)
+	fmt.Println(designs)
+}
+
+func Part2() {
+	fmt.Println("Part 2")
+}
