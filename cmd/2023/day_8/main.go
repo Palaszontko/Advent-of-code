103c5a20bfe973eab72d2c7a82ba06c7b4e3bf62
Olgierd Palasz <olgierd.palasz@gmail.com>
2025-06-18 14:46:39 +0200

    feat: solved day 8 - part 2

--- cmd/2023/day_8/main.go
+++ cmd/2023/day_8/main.go
@@ -10,7 +10,7 @@ import (
 func main() {
 	fmt.Println("Advent of Code 2023!")
 	Part1()
-	// Part2()
+	Part2()
 }
 
 type Graph struct {
@@ -80,4 +80,74 @@ func Part1() {
 
 func Part2() {
 	fmt.Println("Part 2")
+
+	input := utils.ReadFile("cmd/2023/day_8/input.txt")
+
+	// 	input = `LR
+
+	// 11A = (11B, XXX)
+	// 11B = (XXX, 11Z)
+	// 11Z = (11B, XXX)
+	// 22A = (22B, XXX)
+	// 22B = (22C, 22C)
+	// 22C = (22Z, 22Z)
+	// 22Z = (22B, 22B)
+	// XXX = (XXX, XXX)`
+
+	instructions := strings.Split(input, "\n")[0]
+
+	graph := NewGraph()
+
+	startingNodes_A := map[string]string{}
+
+	for _, line := range strings.Split(input, "\n")[2:] {
+		if line != "" {
+			name := strings.TrimSpace(strings.Split(line, "=")[0])
+			values := strings.Split(strings.Split(line, "=")[1][2:len(strings.Split(line, "=")[1])-1], ", ")
+			graph.addEdge(name, values)
+
+			if string(name[2]) == "A" {
+				startingNodes_A[name] = name
+			}
+		}
+	}
+
+	ind := 0
+
+	foundCycle := map[string]bool{}
+	cycleLength := map[string]int64{}
+
+	for key, _ := range startingNodes_A {
+		foundCycle[key] = false
+	}
+
+	data := []int64{}
+
+	for len(startingNodes_A) != 0 {
+		currentInstruction := string(instructions[ind%len(instructions)])
+
+		for key, val := range startingNodes_A {
+			startingNodes_A[key] = graph.get(val, currentInstruction)
+
+			if foundCycle[key] {
+				if string(startingNodes_A[key][2]) == "Z" {
+					data = append(data, cycleLength[key])
+					delete(startingNodes_A, key)
+				} else {
+					cycleLength[key] += 1
+				}
+			} else {
+				if string(startingNodes_A[key][2]) == "Z" {
+					foundCycle[key] = true
+					cycleLength[key] = 1
+				}
+			}
+		}
+		ind += 1
+	}
+
+	result := utils.LcmMultipleNumbers(data)
+
+	fmt.Println(result)
+
 }
