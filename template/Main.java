7c39edfe6a995ef29fa95509a64b60563ed0789a
Olgierd Palasz <olgierd.palasz@gmail.com>
2025-06-24 03:09:55 +0200

    feat: add template java class

---
+++ template/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Advent of Code 2024!");
+        part1();
+        part2();
+    }
+
+    public static void part1() {
+        System.out.println("Part 1");
+    }
+
+    public static void part2() {
+        System.out.println("Part 2");
+    }
+}
